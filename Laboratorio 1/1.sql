603a4f52d409f10d6093dfc845c1df21efe6470e
Daniel Rivera <Danielf.r97@gmail.com>
2019-08-13 13:10:38 -0500

    Punto 1

---
+++ Laboratorio 1/1.sql
@@ -0,0 +1,29 @@
+BEGIN 
+    EXECUTE IMMEDIATE 
+    'CREATE TABLE producto(id NUMBER(8)
+    ,nombre VARCHAR(20)
+    ,descripcion VARCHAR(20)
+    ,productor_id NUMBER(8)
+    ,cantidad NUMBER(4)
+    ,precio NUMBER(8)
+    ,PRIMARY KEY(id)
+    ,FOREIGN KEY(productor_id) REFERENCES PRODUCTOR)'; 
+END;
+
+BEGIN 
+    EXECUTE IMMEDIATE 
+    'CREATE TABLE productor(id NUMBER(8)
+    ,nombre VARCHAR(20)
+    ,direccion VARCHAR(20)
+    ,PRIMARY KEY(id))'; 
+END;
+
+BEGIN 
+    EXECUTE IMMEDIATE 
+    'CREATE TABLE venta(id NUMBER(8)
+    ,producto_id NUMBER(8)
+    ,cantidad NUMBER(4)
+    ,fecha DATE
+    ,PRIMARY KEY(id)
+    ,FOREIGN KEY(producto_id) REFERENCES PRODUCTO)'; 
+END;
