909afe77e5818a2cfd1e7c799e363d88568735df
Daniel Rivera <33033057+Sht97@users.noreply.github.com>
2019-08-19 20:26:19 -0500

    Update 5.sql

--- Laboratorio 1/5.sql
+++ Laboratorio 1/5.sql
@@ -1,8 +1,8 @@
 DECLARE
     numerito NUMBER(4);
     CURSOR productore IS
-    SELECT productor.id as identificacion, productor.nombre as nombre,count(*) AS cantidad 
-        FROM productor  inner join producto on producto.productor_id=productor.id
+    SELECT productor.id as identificacion, productor.nombre as nombre,count(producto.id) AS cantidad 
+        FROM productor  left join producto on producto.productor_id=productor.id
             group by    productor.id,productor.nombre;
 BEGIN
     FOR p IN productore LOOP
