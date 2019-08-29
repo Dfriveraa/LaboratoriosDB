77fd4daae425acff9f194402311b7e2c72a52086
Daniel Rivera <33033057+Sht97@users.noreply.github.com>
2019-08-29 09:37:03 -0500

    Update data.sql

--- Laboratorio 2/data.sql
+++ Laboratorio 2/data.sql
@@ -6,3 +6,7 @@ INSERT INTO cliente (ced, nom) values (4, 'Carlos');
 INSERT INTO empleo (ced, nit_empresa, valor_mensual) values (1, 1, 1500);
 INSERT INTO empleo (ced, nit_empresa, valor_mensual) values (2, 2, 800);
 INSERT INTO empleo (ced, nit_empresa, valor_mensual) values (3, 3, 456);
+
+INSERT INTO GASTO  values (1, 1, 900,'cion');
+INSERT INTO GASTO  values (1, 1, 5000,'crip');
+INSERT INTO GASTO  values (3, 3, 800,'des');
